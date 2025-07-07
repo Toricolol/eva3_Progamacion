6be6daea7201c84bd8ec2e40d849c62fc83d3d7b
nicolas <toricolol12@gmail.com>
2025-07-06 23:37:30 -0400

    commit 2
    ver si me carga lo pongo en la pagina y probando si responde bien

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,22 @@
+'use client';
+import React, { useState } from 'react';
+
+export interface Registro {
+  id: number;
+  nombre: string;
+  edad: string;
+  categoria: string;
+  descripcion: string;
+  fecha: string;
+}
+
+export default function Page() {
+  const [registros, setRegistros] = useState<Registro[]>([]);
+
+  return (
+    <main>
+      <p>App cargada</p>
+    </main>
+  );
+}
 
